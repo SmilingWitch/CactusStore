56e1ca816454392d6da88bd180eb7d8327bccf00
danielay <danielitayero@gmail.com>
2023-06-09 16:41:45 +0200

    feat(components): add cartTottal component

---
+++ src/components/cartTotal/CartTotal.jsx
@@ -0,0 +1,23 @@
+import  './CartTotal.css'
+import CartCard from "../cartCard/CartCard"
+import axios from "axios";
+import { useState , useEffect, Context } from "react";
+
+
+function CartTotal() {
+    return (
+       <div className="cartPP">
+            <div className='petitionsBx'>
+                <div className='gridCart'>
+                <CartCard className = "cardcart"/>           
+                </div>
+            </div> 
+            <div className='order'>
+               Comprar 
+            </div>           
+        </div>
+         
+    );
+}
+
+export default CartTotal ;
